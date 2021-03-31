5dcc979e31d3e717043685add7083a45eec4847f
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2021-03-31 14:44:39 +0200

    Add files to test transformation with Cassandra

---
+++ scripts/dsbulk_load.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DSBULK=dsbulk
+cqlsh -f scripts/schema_raw.cql
+
+echo "Loading tagpacks"
+$DSBULK load -c json -k tagpacks -t tag_by_address -url src/test/resources/cassandra/test_tags.json --schema.allowMissingFields true
+echo "Loading exchange_rates"
+$DSBULK load -c csv -header true -k eth_raw -t exchange_rates -url src/test/resources/cassandra/test_exchange_rates.csv
+echo "Loading blocks"
+$DSBULK load -c csv -header true -k eth_raw -t block -url src/test/resources/cassandra/test_blocks.csv
+echo "Loading transactions"
+$DSBULK load -c csv -header true -k eth_raw -t transaction -url src/test/resources/cassandra/transactions.csv
