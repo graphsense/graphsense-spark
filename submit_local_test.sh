aee3b28619d0fd17bf0cc7867ad3caf826e76f85
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2021-11-29 12:51:20 +0100

    Set version number

--- submit_local_test.sh
+++ submit_local_test.sh
@@ -9,7 +9,7 @@
   --conf spark.driver.host="localhost" \
   --conf spark.sql.extensions=com.datastax.spark.connector.CassandraSparkExtensions \
   --packages com.datastax.spark:spark-cassandra-connector_2.12:3.1.0,org.rogach:scallop_2.12:4.0.2,joda-time:joda-time:2.10.10 \
-  target/scala-2.12/graphsense-ethereum-transformation_2.12-0.5.1-SNAPSHOT.jar \
+  target/scala-2.12/graphsense-ethereum-transformation_2.12-0.5.1.jar \
   --raw-keyspace "eth_raw" \
   --tag-keyspace "tagpacks" \
   --target-keyspace "eth_transformed" \
