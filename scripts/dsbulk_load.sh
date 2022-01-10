2e3e36ae7a61381675b3914c998be1ba425b079c
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2022-01-10 16:30:35 +0100

    Set log directory in test data ingest script

--- scripts/dsbulk_load.sh
+++ scripts/dsbulk_load.sh
@@ -4,12 +4,12 @@ DSBULK=dsbulk
 cqlsh -f scripts/schema_raw.cql
 
 echo "Loading tagpacks"
-$DSBULK load -c json -k tagpacks -t address_tag_by_address -url src/test/resources/cassandra/test_tags.json --schema.allowMissingFields true
+$DSBULK load -logDir /tmp -c json -k tagpacks -t address_tag_by_address -url src/test/resources/cassandra/test_tags.json --schema.allowMissingFields true
 echo "Loading exchange_rates"
-$DSBULK load -c json -k eth_raw -t exchange_rates -url src/test/resources/cassandra/test_exchange_rates.json
+$DSBULK load -logDir /tmp -c json -k eth_raw -t exchange_rates -url src/test/resources/cassandra/test_exchange_rates.json
 echo "Loading blocks"
-$DSBULK load -c csv -header true -k eth_raw -t block -url src/test/resources/cassandra/test_blocks.csv
+$DSBULK load -logDir /tmp -c csv -header true -k eth_raw -t block -url src/test/resources/cassandra/test_blocks.csv
 echo "Loading transactions"
-$DSBULK load -c csv -header true -k eth_raw -t transaction -url src/test/resources/cassandra/transactions.csv --schema.allowMissingFields true
+$DSBULK load -logDir /tmp -c csv -header true -k eth_raw -t transaction -url src/test/resources/cassandra/transactions.csv --schema.allowMissingFields true
 echo "Loading traces"
-$DSBULK load -c csv -header true -k eth_raw -t trace -url src/test/resources/cassandra/traces.csv
+$DSBULK load -logDir /tmp -c csv -header true -k eth_raw -t trace -url src/test/resources/cassandra/traces.csv
