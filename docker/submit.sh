96f93686364ae3db323a5ca6ad1c21875a210266
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2022-03-10 12:26:25 +0100

    Update dependencies

--- docker/submit.sh
+++ docker/submit.sh
@@ -27,7 +27,7 @@ echo -en "Starting Spark job ...\n" \
   --conf spark.driver.memory="64G" \
   --conf spark.sql.session.timeZone=UTC \
   --conf spark.serializer="org.apache.spark.serializer.KryoSerializer" \
-  --packages com.datastax.spark:spark-cassandra-connector_2.12:3.0.1,org.rogach:scallop_2.12:4.0.2,joda-time:joda-time:2.10.10 \
+  --packages com.datastax.spark:spark-cassandra-connector_2.12:3.0.1,org.rogach:scallop_2.12:4.1.0,joda-time:joda-time:2.10.10 \
   target/scala-2.12/graphsense-ethereum-transformation_2.12-0.5.2-SNAPSHOT.jar \
   --raw-keyspace "$RAW_KEYSPACE" \
   --target-keyspace "$TGT_KEYSPACE" \
